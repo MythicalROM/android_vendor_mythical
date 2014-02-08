d4be53215aae46a5195b9401dbe43b3cf642aa42
herna <herna@mokeedev.com>
2014-02-08 15:14:52 +0100

    Add Falcon to vendor

---
+++ products/mythical_falcon.mk
@@ -0,0 +1,39 @@
+# Copyright (C) 2013 ParanoidAndroid Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Check for target product
+ifeq (mythical_falcon,$(TARGET_PRODUCT))
+
+# OVERLAY_TARGET adds overlay asset source
+OVERLAY_TARGET := mythical_xhdpi
+
+# Build paprefs from sources
+PREFS_FROM_SOURCE ?= false
+
+# Inherit telephony common stuff
+$(call inherit-product, vendor/mythical/configs/telephony.mk)
+
+# Include AOSPA common configuration
+include vendor/mythical/main.mk
+
+# Inherit device configuration
+$(call inherit-product, device/motorola/falcon/full_falcon.mk)
+
+# Override AOSP build properties
+PRODUCT_NAME := mythical_falcon
+PRODUCT_BRAND := Motorola
+PRODUCT_MODEL := Motorola
+
+endif
+
